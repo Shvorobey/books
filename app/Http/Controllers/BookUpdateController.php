9167c8a462681a997259d20c1e0c3f7d883275c8
Shvorobey <a.shvorobey@gmail.com>
2019-06-14 00:46:08 +0300

    Well done!

--- app/Http/Controllers/BookUpdateController.php
+++ app/Http/Controllers/BookUpdateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class BookUpdateController extends Controller
@@ -14,6 +15,27 @@ class BookUpdateController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        if ($request->method() == 'POST') {
+            $this->validate($request, [
+                'title' => 'required | max: 100 | string ',
+                'image' => 'image | dimensions: min_width=370, min_height=330 |  file',
+            ]);
+
+            $book = Book::find($request->input('id'));
+            $book->title = $request->input('title');
+
+            $img = $request->img;
+            if ($img) {
+                $imageName = $img->getClientOriginalName();
+                $img->move('images', $imageName);
+                $book->img = 'http://books/images/' . $imageName;
+            }
+            $book->save();
+
+        } else
+        {
+            $book = Book::find($request->get('id'));
+        }
+        return view('admin.book-update', ['book' => $book]);
     }
 }
---
+++ resources/views/admin/book-update.blade.php
@@ -0,0 +1,36 @@
+Вы вошли под именем: <h1 style="color:#0000ff"><strong>{{\Auth::user()->name}}</strong></h1>
+<p><a href="{{route('main')}}">На главную </a>
+
+<form action="" method="post" enctype="multipart/form-data">
+@csrf
+
+    <strong>Название книги:</strong> <br>
+    <input type="text" name="title" placeholder="Не более 100 символов"
+           value="{{old ('title', $book->title )}}"/><br>
+    @if ($errors->any('title'))
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->get('title') as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <hr>
+
+    <strong>Изображение:</strong> <i>370x330</i><strong> :</strong>
+    <input type="file" name="img"/><br>
+    <input type="hidden" name="id" value="{{old('id', $book->id)}}"/>
+    @if ($errors->any('img'))
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->get('img') as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <hr>
+
+    <input type="submit" value="Сохранить"/>
+</form>
